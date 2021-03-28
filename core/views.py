34798070b981577b4dafc8b72e848325317df4b0
atthana-p <atthana.p@codium.co>
2021-03-29 00:19:51 +0700

    13-Overriding the behaviour of the POST method.

--- core/views.py
+++ core/views.py
@@ -19,17 +19,19 @@ class CustomerViewSet(viewsets.ModelViewSet):
     def list(self, request, *args, **kwargs):
         # import pdb
         # pdb.set_trace()
-        customers = Customer.objects.filter(id=3) # พอเรามา override แบบนี้ มันก้อจะสนใจแค่ id=3 นะ แต่ไม่ทำ active=true ข้างบน
+        customers = Customer.objects.filter(
+            id=3)  # พอเรามา override แบบนี้ มันก้อจะสนใจแค่ id=3 นะ แต่ไม่ทำ active=true ข้างบน
         serializer = CustomerSerializer(customers, many=True)
         return Response(serializer.data)
 
-    def retrieve(self, request, *args, **kwargs): # ใช้เมื่อมีการดึงค่า id ออกมา ถ้าพิมพ์ kwargs จะได้ {'pk': '2'} นะใน pdb
+    def retrieve(self, request, *args,
+                 **kwargs):  # ใช้เมื่อมีการดึงค่า id ออกมา ถ้าพิมพ์ kwargs จะได้ {'pk': '2'} นะใน pdb
         # obj = self.get_object()  # สร้าง object ขึั้นมา แล้วใส่เข้าไปให้เป็น serializer จิงๆเราทำเหมือนกับที่เราไป override มานะ
         # serializer = CustomerSerializer(obj)
         # return Response(serializer.data)
         # return Response({'details': 'Not allowed q'})  # คือ เราจะให้มัน return เป็นอะไรก็ได้นะ ผ่านทาง Response แบบนี้
-        return HttpResponseNotAllowed('Q Not allowed')  # หรือจะส่งกลับไปแบบนี้ก้อได้ แบบที่ Django มีมาให้ อันนี้ได้ 405 นะ
-
+        return HttpResponseNotAllowed(
+            'Q Not allowed')  # หรือจะส่งกลับไปแบบนี้ก้อได้ แบบที่ Django มีมาให้ อันนี้ได้ 405 นะ
 
     #  อันนี้แค่ไปก๊อบมาให้ดูว่า ที่เราไป override คือให้ไปทับกับตัวนี้นะ
     # class RetrieveModelMixin:
@@ -42,6 +44,18 @@ class CustomerViewSet(viewsets.ModelViewSet):
     #         serializer = self.get_serializer(instance)
     #         return Response(serializer.data)
 
+    def create(self, request, *args, **kwargs):  # อันนี้เป็นการทำ override create() บ้าง
+        data = request.data
+        customer = Customer.objects.create(
+            name=data['name'], address=data['address'], datasheet_id=data['datasheet']
+        )
+        profession = Profession.objects.get(id=data['profession'])
+
+        customer.profession.add(profession)
+        customer.save()
+
+        serializer = CustomerSerializer(customer)
+        return Response(serializer.data)
 
 
 class ProfessionViewSet(viewsets.ModelViewSet):
