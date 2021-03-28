2dc1c9cc63171965ab6a6a11dc81f985ad9aa3a7
atthana-p <atthana.p@codium.co>
2021-03-28 21:10:55 +0700

    6-Setup of the Django Rest Framework and creating the first API Endpoint.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from core.models import Customer
+from core.serializers import CustomerSerializer
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    # queryset = Customer.objects.filter(active=True)  # ถ้าเราต้องการ filter ก้อมาทำแบบนี้ได้เลย เพราะอันนี้คือการ query
+    serializer_class = CustomerSerializer
