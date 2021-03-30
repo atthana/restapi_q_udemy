656a0fc0bc2ff14e75423c1d4f2921ccc4cebf30
atthana-p <atthana.p@codium.co>
2021-03-30 12:58:58 +0700

    26-StringRelatedFields  กับ  PrimaryKeyRelatedField.

--- core/serializers.py
+++ core/serializers.py
@@ -5,32 +5,38 @@ from .models import Customer, Profession, Document, DataSheet
 
 class CustomerSerializer(serializers.ModelSerializer):
     num_professions = serializers.SerializerMethodField()
+    datasheet = serializers.PrimaryKeyRelatedField(read_only=True)
+    profession = serializers.StringRelatedField(many=True)  # ใช้ many=True ร่วมด้วย ไม่งั้นมันจะไม่โชว์ค่าจิงๆ แต่จะเป็น core.xxx.xxx แทน
+    document_set = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Customer
         fields = ('id', 'name', 'address', 'profession',
                   'datasheet', 'active', 'status_message',
-                  'num_professions',
+                  'num_professions', 'document_set'
                   )
 
     def get_num_professions(self, obj):
         # import pdb; pdb.set_trace()
         return obj.num_professions()
 
+    def get_datasheet(self, obj):
+        return obj.datasheet.description
+
 
 class ProfessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profession
-        fields = ('id', 'description', )
+        fields = ('id', 'description',)
 
 
 class DataSheetSerializer(serializers.ModelSerializer):
     class Meta:
         model = DataSheet
-        fields = ('id', 'description', 'historical_data', )
+        fields = ('id', 'description', 'historical_data',)
 
 
 class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
-        fields = ('id', 'dtype', 'doc_number', 'customer', )
+        fields = ('id', 'dtype', 'doc_number', 'customer',)
